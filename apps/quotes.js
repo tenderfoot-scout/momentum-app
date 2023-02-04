cf1cc11c00aa4d8beecad70f69eb1bb4dcc865e4
tenderfoot-scout <62326842+tenderfoot-scout@users.noreply.github.com>
2023-02-04 23:37:05 +0800

    Implemented localstorage for quotes js Still need to style add quote modal, todo modal

--- apps/quotes.js
+++ apps/quotes.js
@@ -1,23 +1,46 @@
-//----- RANDOM QUOTE GENERATOR -----
-//Generate random quote
-var quotes = [
-    "'May the force be with you'",
-    "'You know nothing, Jon Snow'",
-     "'You're a wizard, Harry'"
-  ]
-  
-  function newQuote() {
-    var randomNum = Math.floor(Math.random() * (quotes.length));
-    document.getElementById('quote-display').innerHTML = quotes[randomNum];
+const quoteModal = document.getElementById('quote-modal-container');
+const openQuoteModalBtn = document.getElementById('open-quote-modal');
+const closeQuoteModalBtn = document.querySelector('.close-quote-modal');
+let emptyInputAlert = document.getElementById('empty-alert');
+let quotes = '';
+
+const quoteList = localStorage.getItem('quotes') ? JSON.parse(localStorage.getItem('quotes')) : [
+  "May the force be with you",
+  "You know nothing, Jon Snow",
+   "You're a wizard, Harry"
+]
+console.log(quoteList);
+
+function generateQuote() {
+  var randomNum = Math.floor(Math.random() * (quoteList.length));
+  document.getElementById('quote-display').innerHTML = quoteList[randomNum];
+}
+
+document.querySelector(".add-quote-btn").addEventListener("click", event => {
+  const quoteInput= document.querySelector("#quote-input");
+  addQuote(quoteInput);
+})
+
+function addQuote(quoteInput) {
+  if (quoteInput.value !== "" && quoteInput.value !== " ") {
+    quoteList.push(quoteInput.value);
+    localStorage.setItem('quotes', JSON.stringify(quoteList));
+    location.reload();
+    //quoteModalClose;
+  } else {
+    emptyInputAlert.style.display = 'inline';
   }
-  
-  //Add new quote
-  function addQuote() {
-    var quotePrompt = prompt('Enter your new quote:', '');
-    if (quotePrompt == null || quotePrompt == "") {
-      alert('user cancelled quote prompt');
-    } else {
-      quotes.push(quotePrompt);
-      console.log(quotes);
-    }
-  }
+}
+
+// MODAL FUNCTIONS
+
+openQuoteModalBtn.addEventListener('click', quoteModalOpen);
+closeQuoteModalBtn.addEventListener('click', quoteModalClose);
+
+function quoteModalOpen() {
+  quoteModal.style.display = 'block';
+}
+
+function quoteModalClose() {
+  quoteModal.style.display = 'none';
+}
